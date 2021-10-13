00f6bd3eb3044989363c1f9e94fedbd463dee2ac
camingout <36473937+camingout@users.noreply.github.com>
2021-10-13 17:53:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, url_for, flash, redirect
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('https://words-list.herokuapp.com/')
 def index():
     # persons = persons.spis
     return render_template('index.html')
