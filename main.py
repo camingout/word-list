a3165a3a86c00d1dcab80ff0d907c7c52e601253
camingout <36473937+camingout@users.noreply.github.com>
2021-10-13 16:22:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, url_for, flash, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    # persons = persons.spis
+    return render_template('index.html')
+    #return "Hello World"
+
+
+@app.route('/result',methods = ['POST', 'GET'])
+def final():
+    if request.method == 'POST':
+        separator = request.form['separator']
+        result = request.form['content']
+        if separator:
+            result = list(set(result.split(f"{separator}")))
+        else:
+            result = list(set(result.split()))
+        #result = list(set(result.split(f"{separator}")))
+        return render_template("result.html", result=result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True) 
